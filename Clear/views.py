6440387b948d3e784ec735fde9b5b696b355c4b6
TRedshaw <toby@redshaw.me>
2022-12-29 12:22:18 +0000

    Fixed template render location in views.py.

--- Clear/views.py
+++ Clear/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 def inhaler(request):
     # Renders the page 'inhaler.html' from the clear file in templates
     # Request is always the first bit
-    return render(request, 'clear/static/templates/clear/main/inhaler.html')
+    return render(request, 'clear/main/inhaler.html')
 
 def pollution(request):
-    return render(request, 'clear/static/templates/clear/main/pollution.html')
+    return render(request, 'clear/main/pollution.html')
 
 def settings(request):
-    return render(request, 'clear/static/templates/clear/main/settings.html')
+    return render(request, 'clear/main/settings.html')
 
 # Create your views here.
