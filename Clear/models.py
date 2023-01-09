ca28677757c4d3d65a957314843df9f2475add69
annabelhassen <ajb220@ic.ac.uk>
2023-01-09 08:22:18 +0000

    Changes to the inhaler database

--- Clear/models.py
+++ Clear/models.py
@@ -122,7 +122,7 @@ class Inhaler(models.Model):
 
 class UserInhaler(models.Model):
     # Adding inhaler type to the inhaler page so user can know which inhaler they are tracking
-    inhaler_type = (
+    inhaler_type = [
         ('Beclametasone_dipropionate', 'Beclametasone dipropionate'),
         ('Ciclesonide', 'Ciclesonide'),
         ('Fluticasone_poprionate', 'Fluticasone poprionate'),
@@ -135,7 +135,7 @@ class UserInhaler(models.Model):
         ('Fluticasone_poprionate_with_formoterol', 'Fluticasone_poprionate_with_formoterol'),
         ('Fluticasone_poprionate_with_salmeterol', 'Fluticasone_poprionate_with_salmeterol'),
         ('Fluticasone_furoate_with_vilanterol', 'Fluticasone_furoate_with_vilanterol'),
-    )
+    ]
     # models.PROTECT works so if a user tries to delete an 'Inhaler' record (the one in quotations) then it wont let you
     # models.CASCADE will delete all related UserInhalers if a UserProfile (user) is deleted
 
